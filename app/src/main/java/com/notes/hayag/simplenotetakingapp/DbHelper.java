3e90db2bdd89f52934c8795f8ae7b99902417914
Adrian L. Hayag <hayag.adrian@gmail.com>
2018-10-31 18:00:46 +0800

    Updated with edit.

--- app/src/main/java/com/notes/hayag/simplenotetakingapp/DbHelper.java
+++ app/src/main/java/com/notes/hayag/simplenotetakingapp/DbHelper.java
@@ -20,34 +20,35 @@ public class DbHelper extends SQLiteOpenHelper {
     public static final String COL_ID = "id";
     public static final String COL_TITLE = "title";
     public static final String COL_BODY = "body";
-    public static final String COL_CREATE_AT = "created_at";
-    public static final String COL_UPDATE_AT = "updated_at";
-    
+    public static final String COL_CREATED_AT = "created_at";
+    public static final String COL_UPDATED_AT = "updated_at";
+
 
     public DbHelper(Context context) {
+
         super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-       String createnotestable = "CREATE TABLE " + TABLE_NOTES  + "(" + 
-               COL_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
-               COL_TITLE + " TEXT, " +
-               COL_BODY + " TEXT, " +
-               COL_CREATE_AT + " TEXT, " +
-               COL_UPDATE_AT + " TEXT" +
-               ")";
+    public void onCreate(SQLiteDatabase db) {
+        String createNotesTable = "CREATE TABLE "+TABLE_NOTES+"(" +
+                COL_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                COL_TITLE + " TEXT, " +
+                COL_BODY + " TEXT, " +
+                COL_CREATED_AT + " TEXT, " +
+                COL_UPDATED_AT + " TEXT" +
+                ")";
 
-       sqLiteDatabase.execSQL(createnotestable);
-
-        Log.d(TAG, createnotestable);
+        db.execSQL(createNotesTable);
+        Log.d(TAG, createNotesTable);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 
+    // inserting a note
     public long insertNote(SQLiteDatabase db, String title, String body){
         Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
@@ -57,28 +58,48 @@ public class DbHelper extends SQLiteOpenHelper {
         ContentValues cv = new ContentValues();
         cv.put(COL_TITLE, title);
         cv.put(COL_BODY, body);
-        cv.put(COL_CREATE_AT, year + "-" + month + "-" + day);
-        cv.put(COL_UPDATE_AT, year + "-" + month + "-" + day);
+        cv.put(COL_CREATED_AT, year + "-" + month + "-" + day);
+        cv.put(COL_UPDATED_AT, year + "-" + month + "-" + day);
 
-        long id = db.insert(TABLE_NOTES, null, cv);
-        return id;
+        return db.insert(TABLE_NOTES, null, cv);
+    }
+
+    public boolean updateNote(SQLiteDatabase db, long id, String title, String body){
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        ContentValues cv = new ContentValues();
+        cv.put(COL_TITLE, title);
+        cv.put(COL_BODY, body);
+        cv.put(COL_UPDATED_AT, year + "-" + month + "-" + day);
+
+        int numberOfRowsAffected = db.update(TABLE_NOTES, cv, COL_ID+"="+id, null);
+        return numberOfRowsAffected > 0;
+    }
+
+    public boolean deleteNote(SQLiteDatabase db, long id){
+        int numberOfRowsDeleted = db.delete(TABLE_NOTES, COL_ID + "=" + id, null);
+        return numberOfRowsDeleted > 0;
     }
 
     public ArrayList<Note> getAllNotes(SQLiteDatabase db){
         ArrayList<Note> notes = new ArrayList<>();
 
-     Cursor cursor = db.query(TABLE_NOTES, null, null, null, null, null, null);
-        while (cursor.moveToNext()){
+        Cursor cursor = db.query(TABLE_NOTES, null, null, null, null, null, null);
+
+        while(cursor.moveToNext()){
             int id = cursor.getInt(cursor.getColumnIndex(COL_ID));
             String title = cursor.getString(cursor.getColumnIndex(COL_TITLE));
-            String body= cursor.getColumnName(cursor.getColumnIndex(COL_BODY));
-            String createdAt = cursor.getString(cursor.getColumnIndex(COL_CREATE_AT));
-            String updatedAt = cursor.getString(cursor.getColumnIndex(COL_UPDATE_AT));
+            String body = cursor.getString(cursor.getColumnIndex(COL_BODY));
+            String createdAt = cursor.getString(cursor.getColumnIndex(COL_CREATED_AT));
+            String updatedAt = cursor.getString(cursor.getColumnIndex(COL_UPDATED_AT));
 
             Note note = new Note(id, title, body, createdAt, updatedAt);
             notes.add(note);
         }
-        return notes;
 
+        return notes;
     }
 }
--- app/src/main/java/com/notes/hayag/simplenotetakingapp/NotesActivity.java
+++ app/src/main/java/com/notes/hayag/simplenotetakingapp/NotesActivity.java
@@ -1,5 +1,6 @@
 package com.notes.hayag.simplenotetakingapp;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,21 +13,33 @@ import android.widget.Toast;
 
 public class NotesActivity extends AppCompatActivity {
 
-
-    public static final String TAG = "MainActivity";
+    private EditText titleEditText = null;
+    private EditText bodyEditText = null;
     private DbHelper dbHelper = null;
     private SQLiteDatabase db = null;
-
-
-
+    private String title;
+    private String body;
+    private long noteId;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
+        titleEditText = findViewById(R.id.title_note);
+        bodyEditText = findViewById(R.id.body_note);
 
         dbHelper = new DbHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        intent = getIntent();
+
+        title = intent.getStringExtra("Title");
+        body = intent.getStringExtra("Body");
+        noteId = intent.getLongExtra("note_id", 1);
+        titleEditText.setText(title);
+        bodyEditText.setText(body);
 
     }
 
@@ -41,19 +54,21 @@ public class NotesActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        EditText title_note = findViewById(R.id.title_note);
-        EditText body_note = findViewById(R.id.body_note);
-        String title = title_note.getText().toString();
-        String body = body_note.getText().toString();
+        if(id == R.id.save_note){
+            if(noteId == 1) {
+                long save = dbHelper.insertNote(db, titleEditText.getText().toString(), bodyEditText.getText().toString());
+                Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+                this.finish();
+            } else {
+                boolean update = dbHelper.updateNote(db, noteId ,titleEditText.getText().toString(), bodyEditText.getText().toString());
+                if (update){
+                    Toast.makeText(this, "Note is updated!", Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }else{
 
-       SQLiteDatabase db = dbHelper.getWritableDatabase();
-       long noteID = dbHelper.insertNote(db, title, body);
-
-       if (noteID > 0){
-           Toast.makeText(this, "Showed", Toast.LENGTH_SHORT).show();
-       }
-
-
+                }
+            }
+        }
         return true;
     }
 }
